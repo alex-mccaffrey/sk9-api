3f6a0405ab335c84fc26248b1153b3976b89ffb8
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-07-17 06:29:45 -0600

    added client origin

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const { CLIENT_ORIGIN } = require("./config");
 const sessionsRouter = require('./sessions/sessions-router')
 const foldersRouter = require('./folders/folders-router')
 
@@ -15,7 +16,11 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 
 
 app.use('/api/folders', foldersRouter)
